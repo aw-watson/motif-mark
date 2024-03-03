70fac0f9f2c103ce979c6768deab3a993bfbaa49
Andre Watson <andre.watson22@gmail.com>
2024-03-03 15:34:48 -0800

    drawing motifs

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -12,6 +12,17 @@ base_mapping = {"A":"A", "C":"C","G":"G","T":"T",
                 "R":"[AG]","Y":"[CT]","B":"[CGT]","D":"[AGT]",
                 "H":"[ACT]","V":"[ACG]","N":"[AGCT]", "U":"T"}
 
+colors = [(1,38/255,0,1),
+          (1,159/255,0,1),
+          (1,221/255,0,1),
+          (159/255,180/255,0,1),
+          (50/255,130/255,0,1),
+          (0,130/255,90/255,1),
+          (0,77/255,132/255,1),
+          (81/255,0,32/255,1),
+          (72/255,38/255,13/255,1),
+          (210/255,180/255,140/255,1)]
+
 def oneline_fasta(infile: str, outfile: str):
     """Takes a FASTA file (infile) and puts all sequence lines on one line. Writes the result to outfile"""
     with open(infile, 'rt') as rf, open(outfile, 'wt') as wf:
@@ -38,34 +49,6 @@ def regex_maker(motif_seq: str) -> str:
     for b in motif_seq:
         motif_regex += base_mapping[b]
     return motif_regex
-
-class Canvas:
-    def __init__(self, n_seq:int):
-        self._seq_amt = float(n_seq)
-        self._surface = cairo.ImageSurface(cairo.FORMAT_RGB16_565, 
-                                           2300, 
-                                           50+150*math.ceil(self._seq_amt/2.0))
-        self._cx = cairo.Context(self._surface)
-        #white background
-        self._cx.set_line_width(1)
-        self._cx.set_source_rgba(1,1,1,1)
-        self._cx.rectangle(0,0,2300,50+150*math.ceil(self._seq_amt/2.0))
-        self._cx.fill()
-        self._xoffset = 0
-        self._yoffset = 0
-    def draw_seq(self):
-        #base position (leftmost position we want to draw at)
-        base_position = (100+ self._xoffset*1100, 50 + self._yoffset*150) #(x,y)
-        #draw intron/exon
-
-        #draw motifs
-        #increment offset to draw next sequence
-        if self._xoffset == 0 and self._yoffset == math.ceil(self._seq_amt/2.0):
-            self.y_offset = 0
-            self.x_offset = 1
-        else:
-            self._yoffset += 1
-        raise NotImplementedError
     
 class _Motif:
     def __init__(self, motif_name:str, motif_start:int, motif_end:int):
@@ -82,7 +65,7 @@ class Sequence:
     def __init__(self, name:str,  nucleotides:str):
         self.name = name
         seq_match = re.fullmatch("[actg]*([ACTG]+)[actg]*", nucleotides)
-        self.exon = None
+        self.exon:_Exon = _Exon(0,0)
         if seq_match is None:
             raise ValueError("Innapropriately formatted reference sequence: expects a single stretch of \
                              uppercase DNA nucleotides (denoting an exon) \
@@ -99,6 +82,71 @@ class Sequence:
         for m in motif_iter:
             #add all matches found to the motifs associated with this sequence
             self.motifs.append(_Motif(motif_seq, m.start(1), m.end(1)))
+
+class Canvas:
+    def __init__(self, n_seq:int):
+        self._surface = cairo.ImageSurface(cairo.FORMAT_RGB16_565, 
+                                           1200, 
+                                           50+250*n_seq)
+        self._cx = cairo.Context(self._surface)
+        #white background
+        self._cx.set_line_width(1)
+        self._cx.set_source_rgba(1,1,1,1)
+        #imagine a 1-column grid of 1000x200 rectangles, with 100 pixels of horizontal padding and 50 pixels of vertical padding
+        self._cx.rectangle(0,0,1200,50+250*n_seq)
+        self._cx.fill()
+        self._cx.set_source_rgba(0,0,0,1)
+        self._yoffset = 0
+    def output(self, name:str):
+        self._surface.write_to_png(f"{name}.png")
+    def draw_seq(self, seq:Sequence):
+        #base position (top left corner of the "rectangle" we're drawing our sequence in)
+        base_position:list[int] = [100, 50 + self._yoffset*250] #(x,y)
+        self._cx.set_source_rgba(0,0,0,1)
+        self._cx.move_to(base_position[0], base_position[1])
+        self._cx.show_text(seq.name)
+        #draw intron
+        self._cx.move_to(base_position[0], base_position[1]+100) #halfway down the left side
+        self._cx.set_line_width(2)
+
+        self._cx.line_to(base_position[0] + len(seq.sequence), base_position[1] + 100) #straight line
+        self._cx.stroke()
+        #draw exon
+        self._cx.move_to(base_position[0] + seq.exon.start, base_position[1]+100) #halfway down the left side, skip intron bases
+        self._cx.set_line_width(10)
+        self._cx.line_to(base_position[0] + seq.exon.end + 1, base_position[1] + 100) #straight line
+        self._cx.stroke()
+        #draw motifs
+        self.draw_motifs(seq, base_position)
+        #increment offset to draw next sequence
+        self._yoffset += 1
+    def draw_motifs(self, seq:Sequence, base_position:list[int]):
+        #v0: no account for overlaps, everything gets drawn over everything
+        motifs_present:set[str]  = set()
+        self._cx.set_line_width(10)
+        for m in seq.motifs:
+            motifs_present.add(m.name)
+            self._cx.set_source_rgba(*motif_color_map[m.name])
+            self._cx.move_to(base_position[0] +  m.start,base_position[1] + 100)
+            self._cx.line_to(base_position[0] + m.end + 1, base_position[1] + 100)
+            self._cx.stroke()
+        self._cx.set_source_rgba(0,0,0,1)
+
+        #legend
+        self._cx.set_line_width(2)
+        for i, m in enumerate(motifs_present):
+            legend_pos = [base_position[0] + 50, base_position[1] + 20 + 10*i]
+            self._cx.set_source_rgba(0,0,0,1)
+            self._cx.rectangle(legend_pos[0],legend_pos[1], 6, 6)
+            self._cx.stroke()
+            self._cx.set_source_rgba(*motif_color_map[m])
+            self._cx.rectangle(legend_pos[0], legend_pos[1], 5 ,5)
+            self._cx.fill()
+            self._cx.set_source_rgba(0,0,0,1)
+            self._cx.move_to(legend_pos[0] + 10,legend_pos[1] + 6)
+            self._cx.show_text(f": {m}")
+
+        #reset 
 
 
 def get_args():
@@ -122,10 +170,23 @@ if __name__ == "__main__":
     os.remove(temp_filename)
 
     #Find all motifs in all sequences
+    n_motifs = 0
+    motif_color_map = {}
     with open(args.motifs, mode='rt') as f:
         for line in f:
+            motif = line.strip().upper()
+            if motif not in motif_color_map:
+                motif_color_map[motif] = colors[n_motifs]
+                n_motifs += 1
             for s in seq_list:
-                s.find_motif(line.strip().upper())
+                s.find_motif(motif)
 
+    #draw sequences
+    cnv: Canvas = Canvas(len(seq_list))
+    for s in seq_list:
+        cnv.draw_seq(s)
+                
+    #output file
+    cnv.output("testing")
 
-    raise NotImplementedError()
+    #raise NotImplementedError()
